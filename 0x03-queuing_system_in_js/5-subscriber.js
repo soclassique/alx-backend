c99616ebd12af9d81332c4563e706783fcd31d5f
Soso <soclassique@yahoo.com>
2023-08-29 00:12:28 +0000

    queueing system

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.on('connect', () => {
+  console.log("Redis client connected to the server");
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  if (channel == 'holberton school channel') {
+    console.log(message)
+  }
+
+  if (message == 'KILL_SERVER') {
+    client.unsubscribe()
+    client.quit()
+  }
+});
